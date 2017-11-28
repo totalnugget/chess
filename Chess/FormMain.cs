fdd8e745aac4fc22f3762770437cf82d9bd28fc0
Jacob van Eijk <jacob.vaneijk@gmail.com>
2017-11-28 11:32:51 +0100

    Initial commit.

---
+++ Chess/FormMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chess
+{
+    public partial class FormMain : Form
+    {
+        [DllImport("User32.dll")]
+        public static extern int SetProcessDPIAware();
+
+        public FormMain()
+        {
+            SetProcessDPIAware();
+            InitializeComponent();
+        }
+    }
+}
