0f331889b0855981c418ce6dbc3c81d913a42f91
Jacob van Eijk <jacob.vaneijk@gmail.com>
2017-12-19 11:01:46 +0100

    Add basic models for a chess game.

---
+++ Chess/Engine/Model/Move.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Engine.Model
+{
+    class Move
+    {
+        public Position StartPosition { get; set; }
+        public Position EndPosition { get; set; }
+
+        Move(Position startPos, Position endPos)
+        {
+            StartPosition = startPos;
+            EndPosition = endPos;
+        }
+    }
+}
---
+++ Chess/Engine/Model/Piece.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Engine.Model
+{
+    class Piece
+    {
+        public PieceColor Color { get; set; }
+        public PieceType Type { get; set; }
+
+        Piece(PieceColor color, PieceType type)
+        {
+            Color = color;
+            Type = type;
+        }
+    }
+
+    enum PieceColor
+    {
+        White,
+        Black
+    }
+
+    enum PieceType
+    {
+        King,
+        Queen,
+        Rook,
+        Bishop,
+        Knight,
+        Pawn
+    }
+}
